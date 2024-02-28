2883886a5196df44de424952e8ed239c3a0877cf
syntax-punk <david.jaeren@gmail.com>
2024-02-28 10:04:08 +0100

    Initial Commit

---
+++ moory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  moory
+//
+//  Created by David on 28/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ moory/mooryApp.swift
@@ -0,0 +1,17 @@
+//
+//  mooryApp.swift
+//  moory
+//
+//  Created by David on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct mooryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
